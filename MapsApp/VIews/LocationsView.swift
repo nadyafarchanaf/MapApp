aac4af760fb89f0c8f45303e5f9d22ded3b08022
nadyafarchanaf <nadyafarcahnaf@gmail.com>
2023-01-28 23:28:31 +0700

    Add Map to SwiftUI project with MapKit

--- MapsApp/VIews/LocationsView.swift
+++ MapsApp/VIews/LocationsView.swift
@@ -6,16 +6,17 @@
 //
 
 import SwiftUI
-
+import MapKit
 
 struct LocationsView: View {
     
     @EnvironmentObject private var vm : LocationsViewModel
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
-            }
+        ZStack {
+            Map(coordinateRegion: $vm.region)
+                .ignoresSafeArea()
         }
     }
 }
